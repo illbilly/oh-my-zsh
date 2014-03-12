121c12945fd0fda695a14fe803dc2ff508d32924
illbilly <chagonkas@gmail.com>
2014-03-12 16:56:09 -0600

    Added ability to specify assignee or project

--- plugins/jira/jira.plugin.zsh
+++ plugins/jira/jira.plugin.zsh
@@ -28,11 +28,34 @@ open_jira_issue () {
     echo "JIRA url is not specified anywhere."
     return 0
   fi
-  if [[ $1 == "-l" ]]; then
-    echo "Retrieving Assigned Projects"
-    IN=$(curl -s -u $JIRA_USER:$JIRA_PASS $jira_url/rest/api/2/search\?jql\=assignee\=$JIRA_USER | json -e 'this.keys = []; for(var i = 0; i< issues.length; i++){keys.push(issues[i].key);}' keys -C)
-    keys=${IN//,/ }
-    echo $keys
+  if [[ $1 == "-la" ]]; then
+    if [ -z "$2" ]; then
+      assignee=$JIRA_USER
+    else
+      assignee=$2
+    fi
+    echo "Retrieving Issues assigned to $assignee"
+    IN=$(curl -s -u $JIRA_USER:$JIRA_PASS $jira_url/rest/api/2/search\?jql\=assignee\=$assignee+order+by+duedate+asc | json -e 'this.keys = []; for(var i = 0; i< issues.length; i++){keys.push(issues[i].key);}' keys -C)
+    if [[ $IN == "evalmachine*" ]]; then
+      echo "Failed to retrieve issues for $assignee"
+    else
+      keys=${IN//,/ }
+      echo $keys
+    fi
+
+  elif [[ $1 == "-lp" ]]; then
+    if [ -z "$2" ]; then
+      echo "Please specify a project key"
+    else
+      echo "Retrieving Issues from project $2"
+      IN=$(curl -s -u $JIRA_USER:$JIRA_PASS $jira_url/rest/api/2/search\?jql\=project\=$2+order+by+duedate+asc | json -e 'this.keys = []; for(var i = 0; i< issues.length; i++){keys.push(issues[i].key);}' keys -C)
+      if [[ $IN == "evalmachine*" ]]; then
+        echo "Failed to retrieve issues for project: $2"
+      else
+        keys=${IN//,/ }
+        echo $keys
+      fi
+    fi
   elif [ -z "$1" ]; then
     echo "Opening new issue"
     $open_cmd "$jira_url/secure/CreateIssue!default.jspa"
