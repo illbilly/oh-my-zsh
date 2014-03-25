f9876fa0a0219f38660d3a01af9d135ab8f68c95
illbilly <chagonkas@gmail.com>
2014-03-25 14:09:47 -0600

    Improved issue searching. Added error handling
    Error handling on issues and comments.  Issues now accept unordered parameters

--- plugins/jira/jira.plugin.zsh
+++ plugins/jira/jira.plugin.zsh
@@ -11,7 +11,6 @@
 # Usage: jira           # opens a new issue
 #        jira ABC-123   # Opens an existing issue
 open_jira_issue () {
-  issuesExec="this.items=[];for(var i=0;i<issues.length;i++){var obj=new Object;obj.summary=issues[i].fields.summary;obj.key=issues[i].key;items.push(obj)}"
   local open_cmd
   if [[ $(uname -s) == 'Darwin' ]]; then
     open_cmd='open'
@@ -29,13 +28,60 @@ open_jira_issue () {
     echo "JIRA url is not specified anywhere."
     return 0
   fi
-    if [[ $1 == "-ls" ]]; then
-      if [ -z "$2" ]; then
-        echo "please provide the status you wish to retrieve"
+    if [[ $1 == "-l" ]]; then
+      echo "Retrieving Issues..." >&2
+      while getopts "laA:S:P:" opt; do
+        case $opt in
+          a)
+            assignee=$JIRA_USER
+            echo "Assignee: $JIRA_USER" >&2
+            ;;
+          A)
+            assignee=$OPTARG
+            echo "Assignee: $OPTARG" >&2
+            ;;
+          S)
+            state=$OPTARG
+            echo "Status: $OPTARG" >&2
+            ;;
+          P)
+            project=$OPTARG
+            echo "Project: $OPTARG" >&2
+            ;;
+          \?)
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+        esac
+      done
+      jql=jql\=
+      if [[ ! -z "$assignee" ]]; then
+        jql+=assignee\=$assignee
+      fi
+      if [[ ! -z "$state" ]]; then
+        t="${jql: -1}"
+        if [[ $t == "=" ]]; then
+          jql+="status=$state"
+        else
+          jql+="+AND+status=$state"
+        fi
+      fi
+      if [[ ! -z "$project" ]]; then
+        t="${jql: -1}"
+        if [[ $t == "=" ]]; then
+          jql+="project=$project"
+        else
+          jql+="+AND+project=$project"
+        fi
+      fi
+      jql+=+order+by+duedate+asc
+      response=$(curl -s -u $JIRA_USER:$JIRA_PASS $jira_url/rest/api/2/search\?$jql)
+      errors=$(underscore --data "$response" select .errorMessages --outfmt text 2>/dev/null)
+      issues=$(underscore --data "$response" extract 'issues' 2>/dev/null)
+      
+      if [[ ! -z "$errors" ]]; then
+        output_errors $errors
       else
-        echo "Retrieve Issues with status: $2"
-        response=$(curl -s -u $JIRA_USER:$JIRA_PASS $jira_url/rest/api/2/search\?jql\=status\=$2+order+by+duedate+asc | underscore extract 'issues') # > response.tmp
-        output_issues $response
+        output_issues $issues
       fi
 
   elif [[ $1 == "-la" ]]; then
@@ -62,8 +108,17 @@ open_jira_issue () {
     else
       if [ -z "$3" ]; then
         echo "Retrieving comments"
-        response=$(curl -s -u $JIRA_USER:$JIRA_PASS $jira_url/rest/api/2/issue/$2/comment?jql=maxResults=5 | underscore extract 'comments')
-        output_comments $response
+        response=$(curl -s -u $JIRA_USER:$JIRA_PASS $jira_url/rest/api/2/issue/$2/comment)
+        errors=$(underscore --data "$response" select .errorMessages --outfmt text 2>/dev/null)
+        comments=$(underscore --data "$response" extract 'comments' 2>/dev/null)
+      
+        if [[ ! -z "$errors" ]]; then
+          output_errors $errors
+        else
+          output_comments $comments
+        fi
+        # response=$(curl -s -u $JIRA_USER:$JIRA_PASS $jira_url/rest/api/2/issue/$2/comment?jql=maxResults=5 | underscore extract 'comments')
+        # output_comments $response
       else
         comment=$(curl -s -u $JIRA_USER:$JIRA_PASS -X POST --data '{"body": "'$3'"}' -H "Content-Type: application/json" $jira_url/rest/api/2/issue/$2/comment | underscore select .body --outfmt text)
         echo "comment posted to $2: $comment"
@@ -86,6 +141,10 @@ open_jira_issue () {
   textreset=$(tput sgr0) # reset the foreground colour
   red=$(tput setaf 1)
   yellow=$(tput setaf 2)
+
+output_errors(){
+  echo "${red}ERROR(s):\t${textreset}$1"
+}
 
 output_issues(){
     keys=$(underscore --data "$1" pluck key --outfmt text)
@@ -117,7 +176,7 @@ output_issues(){
 
 output_comments(){
     echo $1 > test.tmp
-    bodies=$(underscore --data "$1" pluck body | underscore map 'value.replace(/\r?\n|\r/g,"")' --outfmt text)
+    bodies=$(underscore --data "$1" pluck body | underscore map 'value.replace(/\r?\n|\r/g,"")' --outfmt text 2>/dev/null)
     
     bodyArr=()
     while read -r line; do
